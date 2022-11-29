b8701d6b84acc27af02ab20c1c19d64fc6972216
Alex Trinh <atrinh13@terpmail.umd.edu>
2022-11-29 12:46:00 -0500

    Added popup for file rename

--- Individual Components/Screenshot/app/src/main/java/com/example/screenshot/MainActivity.kt
+++ Individual Components/Screenshot/app/src/main/java/com/example/screenshot/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.screenshot
 
+import android.app.AlertDialog
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
@@ -18,11 +21,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     // Reference to Firebase storage
     private var storageRef = storage.reference
 
+    // Button pressed to take screenshot
     private lateinit var button : Button
+
+    private lateinit var builder : AlertDialog.Builder
+
+    private var name : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        builder = AlertDialog.Builder(this)
         button = findViewById(R.id.screenshot)
 
         button.setOnClickListener(this)
@@ -36,16 +46,38 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         saveScreenshot(bitmap)
     }
 
+    private fun getFilename() {
+        builder.setTitle("Name Screenshot")
+        val inflator = LayoutInflater.from(this)
+        val dialogLayout = inflator.inflate(R.layout.popup, null)
+        val userInput = dialogLayout.findViewById<EditText>(R.id.editText)
+        builder.setView(dialogLayout)
+
+        builder.setPositiveButton("Ok") { dialog, i ->
+            name = userInput.text.toString()
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, i ->
+            dialog.cancel()
+        }
+
+        builder.show()
+    }
+
     private fun saveScreenshot(bitmap : Bitmap) {
-        var imagesRef = storageRef.child("images")
-        val baos = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
-        val data = baos.toByteArray()
-        var uploadTask = imagesRef.putBytes(data)
-        uploadTask.addOnFailureListener() {
-            println("Upload failed")
-        }.addOnSuccessListener {
-            println("Upload successful")
+        getFilename()
+        println(name)
+        if (name != null) {
+            var imagesRef = storageRef.child("images/" + name)
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+            var uploadTask = imagesRef.putBytes(data)
+            uploadTask.addOnFailureListener() {
+                println("Upload failed")
+            }.addOnSuccessListener {
+                println("Upload successful")
+            }
         }
     }
 
